e58dc2d6c48c3a1bed86b44f7540b34eb9dd914d
tamimmuheb <99010155+tamimmuheb@users.noreply.github.com>
2022-11-10 13:52:46 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float kmph, mps;
+        /* get the input in km/hr from user */
+        printf("Enter the value in kilometer/hour:");
+        scanf("%f", &kmph);
+
+        /* meter/second conversion */
+        mps = (kmph * 1000) / 3600;
+
+        /* print the output */
+        printf("%.2f km/hr = %.2f m/sec\n", kmph, mps);
+}
