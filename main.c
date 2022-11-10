4d19de45df287d4647b83a1646ef3ced15a6baf8
tamimmuheb <99010155+tamimmuheb@users.noreply.github.com>
2022-11-10 13:54:34 +0600

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 int main()
 {
     float kmph, mps;
-        /* get the input in km/hr from user */
+        /* getting the input in km/hr from user */
         printf("Enter the value in kilometer/hour:");
         scanf("%f", &kmph);
 
         /* meter/second conversion */
         mps = (kmph * 1000) / 3600;
 
-        /* print the output */
+        /* print the output upto two decimal points */
         printf("%.2f km/hr = %.2f m/sec\n", kmph, mps);
 }
